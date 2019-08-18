ee6718847468c555afd41436b21b58af0815011a
leonardosanchez9090 <41757130+leonardosanchez9090@users.noreply.github.com>
2019-08-18 13:57:10 -0500

    Add files via upload

---
+++ practica01/Main.java
@@ -0,0 +1,9 @@
+public class Main{
+	public static void main(String[] args){
+		System.out.println("openjdk 11.0.4 2019-07-16");
+		System.out.println("OpenJDK Runtime Environment (build 11.0.4+11-post-Ubuntu-1ubuntu219.04)");
+		System.out.println("OpenJDK 64-Bit Server VM (build 11.0.4+11-post-Ubuntu-1ubuntu219.04, mixed mode, sharing)");
+		System.out.println("\n");
+		System.out.println("Linux leo-HP-Pavilion-Notebook 5.0.0-25-generic #26-Ubuntu SMP Thu Aug 1 12:04:58 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux");
+		}
+	}
