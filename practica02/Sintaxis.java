706b9592b75175ac5805ca967ed60b8bcd8dd403
leonardosanchez9090 <leonardosanchez9090@ciencias.unam.mx>
2019-08-19 11:13:05 -0500

    Practica

---
+++ practica02/Sintaxis.java
@@ -0,0 +1,33 @@
+import static java.lang.Math.sqrt;
+
+public class Sintaxis {
+
+	public static void main(String[] args) {
+		int a, b, c;
+		b = 20; a = 4; c = -7;
+
+		boolean prop = false;
+		byte ochoBits = 127;
+		short dieciseisBits = -32768;
+
+		double discriminante = b * b - 4 * a * c;
+		double x1 = (-b + sqrt(discriminante)) / (2 * a); 
+		double x2 = (-b - sqrt(discriminante)) / (2 * a);
+
+		char ch1, ch2;
+		ch1= 'g'; ch2 = 'w';
+		int d = 1 / (a - 3);
+
+		// Declaraciones
+		
+		long var_1;
+		var_1 = 2147483648L;
+		
+		char quote;
+		quote = '\'';
+		
+		
+		
+	}
+
+}
